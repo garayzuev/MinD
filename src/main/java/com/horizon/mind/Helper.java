e31736c3f7a6947b8f9d3b4274ffb6d0adf2c288
Garayzuev <garayzuev@gmail.com>
2018-10-08 16:37:24 +0300

    minor refactoring methods for preparing cookie

--- src/main/java/com/horizon/mind/Helper.java
+++ src/main/java/com/horizon/mind/Helper.java
@@ -2,22 +2,31 @@ package com.horizon.mind;
 
 import org.springframework.http.HttpHeaders;
 
+import java.util.List;
+
 import static java.util.Collections.singletonList;
 import static org.springframework.http.HttpHeaders.SET_COOKIE;
 
 public class Helper {
+    private static final String COOKIE_TEMPLATE = "user=%s; Max-Age=63072000; Domain=%s; HttpOnly; Path=/";
+    private static final String DOMAIN = "localhost";
+
     private Helper() {
     }
 
     public static HttpHeaders getCookie(long id) {
         HttpHeaders headers = new HttpHeaders();
-        headers.addAll(SET_COOKIE, singletonList("user=" + Long.toString(id) + "; Max-Age=63072000; Domain=localhost; HttpOnly; Path=/"));
+        headers.addAll(SET_COOKIE, prepareCookie(id));
         return headers;
     }
 
     public static HttpHeaders invalidateCookie() {
         HttpHeaders headers = new HttpHeaders();
-        headers.addAll(SET_COOKIE, singletonList("user=; Max-Age=0; Domain=localhost; HttpOnly; Path=/"));
+        headers.addAll(SET_COOKIE, prepareCookie(null));
         return headers;
+    }
+
+    private static List<String> prepareCookie(Long id) {
+        return singletonList(String.format(COOKIE_TEMPLATE, id, DOMAIN));
     }
 }
